cb0f2b5e6c9cefc806599a48d0e66901d9dde448
Musa Demir <alpermusademir@gmail.com>
2022-12-26 17:11:04 +0300

    all axios functions replaced with isomorphic-unfetch

--- functions/getMovies.js
+++ functions/getMovies.js
@@ -1,56 +1,45 @@
 import React from 'react'
-import axios from 'axios'
+import fetch from 'isomorphic-unfetch'
 
 const apiKey = process.env.NEXT_PUBLIC_TMBD_API_KEY
 const baseURL = process.env.NEXT_PUBLIC_TMDB_BASE_URL
 
-const axiosOptions = {
-  headers: {
-    'Accept-Encoding': '*'
-  }
-}
-
 export const getMovies = (page = 1) => {
-  const data = fetch(`${baseURL}/discover/movie?api_key=${apiKey}&page=${page}`)
-    .then((r) => r.json())
-    .then((data) => {
-      return data
-    })
-
-  return data
+    return fetch(`${baseURL}/discover/movie?api_key=${apiKey}&page=${page}`)
+      .then((r) => r.json())
+      .then((data) => {
+          return data
+      })
 }
 
 export const getPopularMovies = async () => {
-  return (
-    await axios.get(`${baseURL}/movie/popular?api_key=${apiKey}`, {
-      headers: {
-        'Accept-Encoding': '*'
-      }
-    })
-  ).data
+    return fetch(`${baseURL}/movie/popular?api_key=${apiKey}`)
+      .then((r) => r.json())
+      .then((data) => {
+          return data
+      })
 }
 
 export const getMovie = async (id) => {
-  return (
-    await axios.get(`${baseURL}/movie/${id}?api_key=${apiKey}`, {
-      headers: {
-        'Accept-Encoding': '*'
-      }
-    })
-  ).data
+    return fetch(`${baseURL}/movie/${id}?api_key=${apiKey}`)
+      .then((r) => r.json())
+      .then((data) => {
+          return data
+      })
 }
 
 export const getMovieCredits = async (id) => {
-  return (
-    await axios.get(`${baseURL}/movie/${id}/credits?api_key=${apiKey}`, {
-      headers: {
-        'Accept-Encoding': '*'
-      }
-    })
-  ).data
+    return fetch(`${baseURL}/movie/${id}/credits?api_key=${apiKey}`)
+      .then((r) => r.json())
+      .then((data) => {
+          return data
+      })
 }
 
 export const getSimilarMovies = async (id) => {
-  return (await axios.get(`${baseURL}/movie/${id}/similar?api_key=${apiKey}`))
-    .data
+    return fetch(`${baseURL}/movie/${id}/similar?api_key=${apiKey}`)
+      .then((r) => r.json())
+      .then((data) => {
+          return data
+      })
 }
