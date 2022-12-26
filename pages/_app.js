dd29702ce2bcbc099d42ca6b689a95408f4718d4
Musa Demir <alpermusademir@gmail.com>
2022-12-26 22:48:23 +0300

    nextjs-progressbar implemented.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'next-themes'
-import { fucshia } from 'tailwindcss/colors'
+import NextNProgress from 'nextjs-progressbar'
+import { red } from 'tailwindcss/colors'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <NextNProgress color={red[600]} height={4}/>
       <Component {...pageProps} />
     </ThemeProvider>
   )
