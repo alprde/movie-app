7a09175e35f1462edf6f1a29168efb56ce406b7f
Musa Demir <alpermusademir@gmail.com>
2023-01-03 20:12:31 +0300

    active link class added

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 const Navigation = () => {
+  const router = useRouter()
+  const linkClass = 'hover:text-gray-700 dark:hover:text-white'
+  const activeLinkClass = 'text-gray-700 dark:text-white font-semibold'
+
+  const isActive = (path) => {
+    if(router.pathname.startsWith(path)){
+      return activeLinkClass;
+    }else{
+      return linkClass
+    }
+  }
+
   return (
     <section>
       <nav className="flex space-x-6 text-gray-400 font-medium">
-        <a href="/tv-series" className="hover:text-gray-700 dark:hover:text-white">
+        <Link href="/tv-series" className={isActive('/tv-series')}>
           TV Series
-        </a>
-        <Link href="/movies" className="text-gray-700 dark:text-white font-semibold">
+        </Link>
+        <Link href="/movies" className={isActive('/movies')}>
           Movies
         </Link>
-        <a href="/animes" className="hover:text-gray-700 dark:hover:text-white ">
-          Animes
-        </a>
       </nav>
     </section>
   )
